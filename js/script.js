71edbff226fc121bc472da8647bc7b45f7b3e19f
Salvatore Tidu <salvatoret9@outlook.it>
2021-03-18 16:27:17 +0100

    Vue_Hello

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var root = new Vue({
+
+    el: "#root",
+
+    data: {
+
+        message: "VueJs",
+
+        title_style: "text-align: center; font-size: 100px; color: rgb(50,71,91); text-shadow: 2px 2px rgb(63,178,127)",
+
+        imgPath: "https://39ntbr6g.media.zestyio.com/1200px-Vue.js_Logo_2.svg.f1cb27a519bdb5b6ed34049a5b86e317.png",
+
+        logo_style: "display: block; margin: 0 auto; width: 35%"
+
+    }
+
+})
