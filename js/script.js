7852f56a404d6ffe993a9f6b383dd29078ba2200
Salvatore Tidu <salvatoret9@outlook.it>
2021-03-18 16:56:44 +0100

    Vue_Hello_(Aggiornamento_script)

--- js/script.js
+++ js/script.js
@@ -6,11 +6,32 @@ var root = new Vue({
 
         message: "VueJs",
 
-        title_style: "text-align: center; font-size: 100px; color: rgb(50,71,91); text-shadow: 2px 2px rgb(63,178,127)",
+        // title_style: "text-align: center; font-size: 100px; color: rgb(50,71,91); text-shadow: 2px 2px rgb(63,178,127)",
+
+        title_style : {
+
+            textAlign: "center",
+
+            fontSize: "100px",
+
+            color: "rgb(50,71,91)",
+
+            textShadow: "2px 2px rgb(63,178,127)"
+
+        },
 
         imgPath: "https://39ntbr6g.media.zestyio.com/1200px-Vue.js_Logo_2.svg.f1cb27a519bdb5b6ed34049a5b86e317.png",
 
-        logo_style: "display: block; margin: 0 auto; width: 35%"
+        // logo_style: "display: block; margin: 0 auto; width: 35%"
+
+        logo_style: {
+
+            display: "block",
+
+            margin: "0 auto",
+
+            width: "35%"
+        }
 
     }
 
